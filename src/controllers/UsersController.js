33c8a996c6e8c95eff23b195235ee61d77b8d8ac
Viviane Martini <martini.viviane@gmail.com>
2023-09-25 17:10:32 -0300

    tratando de error

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,11 @@
+const AppError = require('../utils/AppError')
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
+
+    if(!name){
+      throw new AppError('Nome é obriga´torio')
+    }
 
     response.status(201).json({ name, email, password })
   }
