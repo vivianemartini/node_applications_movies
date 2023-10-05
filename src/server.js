23544ff330997ae6cc61a72d1e49a436e30f1597
Viviane Martini <martini.viviane@gmail.com>
2023-10-04 21:29:47 -0300

    databse

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 require('express-async-errors')
-
+const database = require('./database/sqlite')
 const AppError = require('./utils/AppError')
-
 const express = require('express')
-
 const routes = require('./routes')
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+database()
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
